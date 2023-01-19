de5253176541f8f56516f9028f9a5ca5aa752593
Mustafa Bursali <m.bursali16@gmail.com>
2023-01-20 00:20:34 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+
+function App() {
+ 
+  return(
+    <div>
+      <div className='wrapper'>
+        <header>
+          <img src="sky3.jpg" className='back' />
+          <img src="anyrgb.com (3).png" className='front' />
+          <h1> WELCOME </h1>
+        </header>
+        <section>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pretium vitae nunc in porttitor. Etiam dictum luctus tellus, ac sagittis ex molestie at. Nullam vehicula non ipsum sit amet maximus. Donec laoreet mollis neque, ut vestibulum tortor. Aliquam tincidunt consequat nisl, nec convallis metus tempus non. Etiam et viverra risus. Vivamus eu massa ante.
+
+Nunc eu erat sit amet arcu blandit sodales. Nullam vel eros a nulla consectetur porta. Pellentesque diam sapien, tempor id diam sit amet, ullamcorper gravida nunc. Pellentesque eu cursus mauris. Proin ac libero at leo laoreet ornare. Nam eu dolor id tellus maximus varius. Aenean fringilla augue vitae nibh ullamcorper, at feugiat arcu tristique. Aliquam interdum dolor sit amet lectus viverra, aliquet auctor ipsum maximus. Nullam vitae sapien in est elementum feugiat. Fusce efficitur sed nunc vel rutrum. Vestibulum quis condimentum massa, ac pretium arcu.
+
+Suspendisse ut enim justo. Donec luctus, nunc tincidunt sodales volutpat, urna nisl consequat ante, id consequat purus felis eu erat. Quisque in libero et augue sollicitudin consequat nec a massa. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam sit amet lectus nec enim aliquam pretium. Aenean in mauris orci. Donec vitae pharetra sapien, quis dictum neque. Ut vel orci facilisis, pellentesque odio nec, imperdiet lectus. Sed lobortis in augue in lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur a accumsan mauris. Integer suscipit tempus neque, eget elementum nunc imperdiet in. Vestibulum tincidunt non nulla fringilla hendrerit. Cras nec mollis augue. Donec tristique lorem ac ipsum malesuada porttitor vitae eu urna.
+        </section>
+        <footer>
+          Github: mbursali93
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export default App;
